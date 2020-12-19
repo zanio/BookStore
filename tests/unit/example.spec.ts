fd15a8a8747b74dc6a1e3ad242cbe619e3996d2c
ANIEFIOK AKPAN <akp.ani@yahoo.com>
2020-12-19 01:00:28 +0100

    chore(tsconfig):edit tsconfig file and spec file

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,6 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
 
+//todo: test is currently failing
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "Welcome to Vuetify";
